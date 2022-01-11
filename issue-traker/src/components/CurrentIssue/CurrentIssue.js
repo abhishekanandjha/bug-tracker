5c6f3a7a27062f7af3f3a6ed7acf9fc79ec900a4
Abhishek Anand Jha <abhishekanandjha48@gmail.com>
2022-01-12 00:53:49 +0530

    Initial commit

---
+++ issue-traker/src/components/CurrentIssue/CurrentIssue.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect} from "react";
+
+import "./CurrentIssue.css";
+
+function CurrentIssue() {
+  const [addIssue, setAddIssue]= useState(true);
+
+  return (
+    <div className="current-issues">
+      <div className="indv-issue">
+        <p className="close-issue">close Issue</p>
+        <p>
+          Assigned<span>Username</span>
+        </p>
+        <p>
+          Priority <span>High</span></p>
+        <p>
+          Discription
+          </p>
+        <p className="description-text">
+          Short Discription of the project describing the issue.Ignore any 
+          spelling mistakes in here.
+        </p>
+        <hr/>
+      </div>
+      <div className="indv-issue">
+        <p className="close-issue">close Issue</p>
+        <p>
+          Assigned<span>Username</span>
+        </p>
+        <p>
+          Priority <span>High</span></p>
+        <p>
+          Discription
+          </p>
+        <p className="description-text">
+          Short Discription of the project describing the issue.Ignore any 
+          spelling mistakes in here.
+        </p>
+        <hr/>
+      </div>
+      <div className="indv-issue">
+        <p className="close-issue">close Issue</p>
+        <p>
+          Assigned<span>Username</span>
+        </p>
+        <p>
+          Priority <span>High</span></p>
+        <p>
+          Discription
+          </p>
+        <p className="description-text">
+          Short Discription of the project describing the issue.Ignore any 
+          spelling mistakes in here.
+        </p>
+        <hr/>
+      </div>
+      
+    </div>
+  );
+}
+
+export default CurrentIssue;
