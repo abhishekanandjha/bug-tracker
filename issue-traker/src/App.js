742a6a0cb0ee2c5e96093a45474c24403a7e78ae
Abhishek Anand Jha <abhishekanandjha48@gmail.com>
2022-01-12 04:07:04 +0530

    removed unused tags

--- issue-traker/src/App.js
+++ issue-traker/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState} from "react";
 import AddIssue from "./components/AddIssue/AddIsssue";
 import CurrentIssue from "./components/CurrentIssue/CurrentIssue";
 
